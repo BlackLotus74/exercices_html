1d5c2047e20d9ef0973de756c02b63e6a81abcb4
François <francoislancian@gmail.com>
2019-07-30 16:26:53 +0200

    Create test.php
    30/07/2019

---
+++ test.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Ma page d'accueil </title>
+    </head>
+    <body>
+        <h1>Bienvenue sur le site de toto </h1>
+        <p> Toto fait de l'anglais :</p>
+        <?php
+        echo '<p>Hello ! What is the day today ? It is '.date("l").' !</p>';
+        ?>
+    </body>
+</html>
